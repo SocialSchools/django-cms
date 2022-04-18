7894ece1bd2375361e65cfa6b28679c439ea23b7
Vinit Kumar <vinit.kumar@kidskonnect.nl>
2022-04-18 15:48:06 +0530

    fix: install issue from github

--- cms/__init__.py
+++ cms/__init__.py
@@ -1,6 +1,7 @@
-import django
-
+# import django
+# this break when we are installing via github, we will switch to the one from pypi
+# from 3.11.0
 __version__ = '3.10.0'
 
-if django.VERSION < (3, 2):
-    default_app_config = 'cms.apps.CMSConfig'
+# if django.VERSION < (3, 2):
+default_app_config = 'cms.apps.CMSConfig'
