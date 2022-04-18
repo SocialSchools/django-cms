0a0b9268228fa63976512ebdec7c768dfcaea102
Vinit Kumar <vinit.kumar@kidskonnect.nl>
2022-04-18 15:50:08 +0530

    bundle assets

--- cms/__init__.py
+++ cms/__init__.py
@@ -1,7 +1,7 @@
 # import django
 # this break when we are installing via github, we will switch to the one from pypi
 # from 3.11.0
-__version__ = '3.10.0'
+__version__ = '3.10.3'
 
 # if django.VERSION < (3, 2):
 default_app_config = 'cms.apps.CMSConfig'
