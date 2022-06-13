f70b6cf74dad887d99c95942fd66cd35d9fa1c76
Vinit Kumar <vinit.kumar@kidskonnect.nl>
2022-06-13 21:16:40 +0530

    feat: bump version and generate assets for 3.10.5

--- cms/__init__.py
+++ cms/__init__.py
@@ -1,6 +1,3 @@
-import django
+__version__ = '3.10.5'
 
-__version__ = '3.10.0'
-
-if django.VERSION < (3, 2):
-    default_app_config = 'cms.apps.CMSConfig'
+default_app_config = 'cms.apps.CMSConfig'
